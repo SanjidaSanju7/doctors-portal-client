87e3298f88458af7519ecc0fd62fab09e5396531
SanjidaSanju7 <sanjidaakter6789@gmail.com>
2022-11-14 20:54:26 +0600

    all home pages created

---
+++ src/Pages/Home/Home/Home.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import About from '../About/About';
+import Banner from '../Banner/Banner';
+import InfoCards from '../InfoCards/InfoCards';
+import MakeAppointment from '../MakeAppointment/MakeAppointment';
+import Services from '../Services/Services';
+import Testimonial from '../Testimonial/Testimonial';
+
+const Home = () => {
+    return (
+        <div>
+            <Banner></Banner>
+            <InfoCards></InfoCards>
+            <Services></Services>
+            <About></About>
+            <MakeAppointment></MakeAppointment>
+            <Testimonial></Testimonial>
+        </div>
+    );
+};
+
+export default Home;
